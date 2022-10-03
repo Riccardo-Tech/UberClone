566c598607ed8b37d31580892d7ab84d27dc0fce
Riccardo-Tech <riccardocarizzoni@gmail.com>
2022-10-03 15:30:17 +0200

    commit Prova

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Babba bia </Text>
+      <Text> Babba bia! </Text>
       <StatusBar style="auto" />
     </View>
   );
