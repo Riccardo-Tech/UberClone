e31f4f1e7405e3d502bad37086550dcf1cd70f59
Riccardo-Tech <riccardocarizzoni@gmail.com>
2022-10-03 16:01:12 +0200

    Prova commit nuova branch

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Babba bia ciao bella</Text>
+      <Text> Babba bia!</Text>
       <StatusBar style="auto" />
     </View>
   );
